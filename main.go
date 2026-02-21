a3d5cb3996f1c0f4f326dacdda6ddc7c815264c4
samiyonas <samuel.x.yonas@gmail.com>
2026-02-21 22:58:36 +0300

    refactored main.go: switched to log probability, added ham and spam bag of words

--- main.go
+++ main.go
@@ -6,47 +6,109 @@ import (
 	"strings"
 	"path/filepath"
 	"io/fs"
+	"math"
 )
 
-func tokenize(root string) map[string]int {
-	BagOfWords := map[string]int{}
-	filepath.WalkDir(root, func (path string, d fs.DirEntry, err error) (error) {
+func tokenize(message string) []string {
+	tokens := strings.Fields(message)
+	for i, val := range tokens {
+		tokens[i] = strings.ToUpper(val)
+	}
+
+	return tokens
+}
+
+func fileToBow(path string, bow map[string]int) (error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	tokens := tokenize(string(content))
+	for _, token := range tokens {
+		bow[token] += 1
+	}
+
+	return nil
+}
+
+func BagOfWords(root string) (map[string]int, error) {
+	bow := map[string]int{}
+	err := filepath.WalkDir(root, func (path string, d fs.DirEntry, err error) (error) {
+		if err != nil {
+			return err
+		}
+
 		if d.IsDir() {
 			return nil
 		}
-		
-		content, err := os.ReadFile(path)
+
+		err = fileToBow(path, bow)
 		if err != nil {
-			panic("couldn't open the file")
-		}
-
-
-		tokens := strings.Fields(string(content))
-		for _, token := range tokens {
-			BagOfWords[strings.ToUpper(token)] += 1
+			return err
 		}
 
 		return nil
-
-		/*
-		for token, freq := range BagOfWords {
-			fmt.Printf("|%s| => %f\n", token, float64(freq)/float64(totalCount));
-		}
-		*/
 	})
-	return BagOfWords
+	if err != nil {
+		return map[string]int{}, err
+	}
+	return bow, nil
 }
 
 func main() {
-	ham_tokens := tokenize("./enron1/")
-	/*
-	totalCount := 0
-	for _, freq := range spam_tokens {
-		totalCount += freq
+	BowHam, err := BagOfWords("./enron1/ham/")
+	if err != nil {
+		panic(err)
 	}
-	*/
+	BowSpam, err := BagOfWords("./enron1/spam/")
+	if err != nil {
+		panic(err)
+	}
 
-	for token, freq := range ham_tokens {
-		fmt.Printf("|%s| => %d\n", token, freq);
+	totalHam := 0
+	for _, freq := range BowHam {
+		totalHam += freq
 	}
+
+	totalSpam := 0
+	for _, freq := range BowSpam {
+		totalSpam += freq
+	}
+
+	EmailBow := map[string]int{}
+
+	err = fileToBow("./enron2/ham/0004.1999-12-10.kaminski.ham.txt", EmailBow)
+	if err != nil {
+		panic(err)
+	}
+
+	prob_of_doc_ham := 0.0
+	prob_of_doc_spam := 0.0
+
+	for token := range EmailBow {
+		if BowHam[token] == 0 {
+			fmt.Printf("Ignored %v\n", token)
+			continue
+		}
+		p := math.Log(float64(BowHam[token])/float64(totalHam))
+		prob_of_doc_ham += p
+		fmt.Printf("%v => %v\n", token, p)
+	}
+
+	for token := range EmailBow {
+		if BowSpam[token] == 0 {
+			fmt.Printf("Ignored %v\n", token)
+			continue
+		}
+		p := math.Log(float64(BowSpam[token])/float64(totalSpam))
+		prob_of_doc_spam += p
+		fmt.Printf("%v => %v\n", token, p)
+	}
+
+	fmt.Printf("probability of document(HAM): %v\n", prob_of_doc_ham)
+	fmt.Printf("probability of document(SPAM): %v\n", prob_of_doc_spam)
+
+	fmt.Printf("len(ham) == %v\n", len(BowHam))
+	fmt.Printf("len(spam) == %v\n", len(BowSpam))
 }
